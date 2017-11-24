b02bc7bea6a506beac59a309e3feb5cca55e3713
habpy <harryaboer@gmail.com>
2017-11-24 16:16:03 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# mamgiota
+
+Small project to implement Masked Authenticated Messaging on the IOTA tangle with Golang.
+
+This project is still under construction (see TODO) so it's not functioning yet.
+
+# Install
+It is assumed that you have Golang installed. You also need to install the Go library API for IOTA which you can download at:
+```
+$ go get -u github.com/iotaledger/giota
+```
+After which you can download `send-message` from
+```
+$ go get -u github.com/habpygo/mamgoiota
+```
+
+# TODO
+Currently there is an error message `Invalid transaction hash` when sending the messasge to the tangel; however, this could be due to a bug in the `iota.lib.go` library.
---
+++ send.go
@@ -0,0 +1,97 @@
+/*
+MIT License
+Copyright (c) 2017 Harry Boer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/iotaledger/giota"
+	"github.com/iotaledger/send-message/stringutils"
+)
+
+//These nodes were working during the demo in Amsterdam 25 October, 2017. Currently only node01 is active
+//var nodes = [4]string{"http://node01.iotameetup.nl:14265", "http://node02.iotameetup.nl:14265", "http://node03.iotameetup.nl:14265", "http://node04.iotatoken.nl:14265"}
+
+//Below the address of http://node01.iotameetup.nl:1337/ where to send the MAM to
+//fmt.Println() statements are for debugging purposes
+var address = "XHBQNNJB9ESMBABXJVVRLXTKXTKOINIJCXOEHIMOJIGLOCPFXYCZGVTHK9RBQWECIXGOKLYFMOXRPYBPWVZG9B9LTZ"
+var seed = ""
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano()) // We need this to avoid ending up with the same sequence. Comment out for debugging.
+
+	trits := "9ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	message := "IOTA World from Golang, do you read?"
+
+	// Select a random seed
+	for i := 0; i < 81; i++ {
+		x := rand.Float64() * 27
+		y := int(x)
+		seed += string(trits[y])
+	}
+	seedTrytes, _ := giota.ToTrytes(seed)
+	fmt.Println("The seed is: ", seedTrytes)
+
+	// Select a random host when all the hosts are actively listening
+	// x := rand.Float64() * 4
+	// y := int(x)
+	//host := nodes[y]
+	provider := "http://node01.iotameetup.nl:14265"
+
+	api := giota.NewAPI(provider, nil)
+	fmt.Println("api is: ", api)
+
+	// Transform the message to tryte values suitable to send MAM's
+	msg := stringutils.ToMAMTrytes(message)
+
+	// Convert the string address to giota address
+	address, err := giota.ToAddress(address)
+	if err != nil {
+		fmt.Println("error in address: ", address)
+	}
+	//fmt.Println("The address is: ", address)
+
+	trs := []giota.Transfer{
+		giota.Transfer{
+			Address: address,
+			Value:   0,
+			Message: msg,
+			Tag:     "",
+		},
+	}
+
+	fmt.Println("trs is: ", trs)
+
+	//Uncomment two lines below if you want to check the message
+	//stringMessage := stringutils.FromMAMTrytes(msg)
+	//fmt.Println("stringMessage is: ", stringMessage)
+
+	bdl, txErr := giota.Send(api, seedTrytes, 9, trs, 14, nil)
+	if txErr != nil {
+		fmt.Println("Error while sending Trytes: ", txErr)
+	}
+	fmt.Println("The bundle is: ", bdl)
+
+}
